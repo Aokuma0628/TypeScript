e3581efc4614abd64633c0ed673ae2ee62bc6c4d
TakeruNishimura <otyazuke1192@gmail.com>
2021-05-16 23:03:28 +0900

    ３章練習問題

--- oreilly/dist/cp3/index.js
+++ oreilly/dist/cp3/index.js
@@ -1,3 +1,12 @@
 "use strict";
-console.log("aaa");
+function cp3() {
+    let a = 1042;
+    let b = 'apples and oranges';
+    const c = 'pineaplles';
+    let d = [true, true, false];
+    let e = { type: 'ficus' };
+    let f = [1, false];
+    const g = [3];
+    let h = null;
+}
 //# sourceMappingURL=index.js.map
