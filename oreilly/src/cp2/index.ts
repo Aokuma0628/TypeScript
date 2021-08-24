28edc6e902bb1772a4a20394c85dd3a13890a3d0
TakeruNishimura <otyazuke1192@gmail.com>
2021-08-24 22:34:49 +0900

    ２章の復習

--- oreilly/src/cp2/index.ts
+++ oreilly/src/cp2/index.ts
@@ -1,9 +1,12 @@
 console.log('Hello TypeScript!')
 
-let a = 1 + 2
-let b = a + 3
-let c = {
+let a: number = 1 + 2;
+let b: number = a + 3;
+let c: {
+	apple: number,
+	banana: number
+} = {
 	apple: a,
 	banana: b
-}
-let d = c.apple * 4
+};
+let d: number = c.apple * 4;
