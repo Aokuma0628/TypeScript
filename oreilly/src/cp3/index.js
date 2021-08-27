1eb3d61c7deb6e208666d655aaaf9d306b6b42bf
TakeruNishimura <otyazuke1192@gmail.com>
2021-08-27 23:54:16 +0900

    ３章復習

---
+++ oreilly/src/cp3/index.js
@@ -0,0 +1,18 @@
+var cp3 = /** @class */ (function () {
+    function cp3() {
+        //do nothing
+    }
+    cp3.prototype.test = function () {
+        var a = 1043;
+        var b = 'apples and oranges';
+        var c = 'pineaplles';
+        var d = [true, true, false];
+        var e = { type: 'ficus' };
+        var f = [1, false];
+        var g = [3];
+        var h = null;
+    };
+    return cp3;
+}());
+var cp = new cp3();
+cp.test();
